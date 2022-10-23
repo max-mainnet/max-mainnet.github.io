163291d0e2114bcfde54af7547e57fa2b4880d7f
Max <max@ref.finance>
2022-10-23 20:42:23 +0800

    link local ref-sdk

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,21 +1,12 @@
+import "react-app-polyfill/ie11";
 import * as React from "react";
-
-import "../styles/main.pcss";
-
-import Card from "../components/Card/index";
-
-import cardImage from "url:../../static/vitalik-vynarchyk-0PePaKnEmuM-unsplash.jpg";
-
-export const App = () => (
-  <div className="h-screen flex items-center justify-center">
-    <Card
-      title="Parcel+TypeScript+React+Tailwind Template"
-      image={cardImage}
-      imageAlt="Architecture"
-      description={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Fusce sit amet mattis augue. Pellentesque tempor tortor eu diam efficitur hendrerit a ac sem. 
-Mauris sit amet tellus cursus, convallis diam.`}
-      tags={["#parcel", "#typescript", "#react", "#tailwind"]}
-    />
-  </div>
-);
+import * as ReactDOM from "react-dom";
+import { WalletSelectorContextProvider } from "./WalletSelectorContext";
+import { Content } from "./Content";
+export const App = () => {
+    return (
+        <WalletSelectorContextProvider>
+            <Content />
+        </WalletSelectorContextProvider>
+    );
+};

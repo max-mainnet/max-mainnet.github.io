ef169c0a68821439dbef858569587266007c4587
Max <max@ref.finance>
2022-11-02 10:28:04 +0800

    deploy mainnet

--- src/views/Content.tsx
+++ src/views/Content.tsx
@@ -7,70 +7,72 @@ import { SignAndSendTransactionsParams } from "@near-wallet-selector/core/lib/wa
 import { useEffect } from "react";
 
 export const Content = () => {
-    const { modal, selector, accountId } = useWalletSelector();
+  const { modal, selector, accountId } = useWalletSelector();
 
-    const onDisConnect = async () => {
-        const wallet = await selector.wallet();
-        return await wallet.signOut();
-    };
+  const onDisConnect = async () => {
+    const wallet = await selector.wallet();
+    return await wallet.signOut();
+  };
 
-    const onConnect = () => {
-        modal.show();
-    };
+  const onConnect = () => {
+    modal.show();
+  };
 
-    const [swapState, setSwapState] = React.useState<"success" | "fail" | null>(null);
+  const [swapState, setSwapState] = React.useState<"success" | "fail" | null>(null);
 
-    const [tx, setTx] = React.useState<string | undefined>(undefined);
+  const [tx, setTx] = React.useState<string | undefined>(undefined);
 
-    React.useEffect(() => {
-        const errorCode = new URLSearchParams(window.location.search).get("errorCode");
+  React.useEffect(() => {
+    const errorCode = new URLSearchParams(window.location.search).get("errorCode");
 
-        const transactions = new URLSearchParams(window.location.search).get("transactionHashes");
+    const transactions = new URLSearchParams(window.location.search).get("transactionHashes");
 
-        const lastTX = transactions?.split(",").pop();
+    const lastTX = transactions?.split(",").pop();
 
-        setTx(lastTX);
+    setTx(lastTX);
 
-        setSwapState(!!errorCode ? "fail" : !!lastTX ? "success" : null);
+    setSwapState(!!errorCode ? "fail" : !!lastTX ? "success" : null);
 
-        window.history.replaceState({}, "", window.location.origin + window.location.pathname);
-    }, []);
+    window.history.replaceState({}, "", window.location.origin + window.location.pathname);
+  }, []);
 
-    const onSwap = async (transactionsRef: Transaction[]) => {
-        const wallet = await selector.wallet();
+  const onSwap = async (transactionsRef: Transaction[]) => {
+    const wallet = await selector.wallet();
 
-        if (!accountId) throw NotLoginError;
+    if (!accountId) throw NotLoginError;
 
-        const WalletSelectorTransactions = {
-            transactions: transformTransactions(transactionsRef, accountId),
-        } as SignAndSendTransactionsParams;
+    const WalletSelectorTransactions = {
+      transactions: transformTransactions(transactionsRef, accountId),
+    } as SignAndSendTransactionsParams;
 
-        return wallet.signAndSendTransactions(WalletSelectorTransactions);
-    };
+    return wallet.signAndSendTransactions(WalletSelectorTransactions);
+  };
 
-    const defaultList = getDefaultTokenList();
+  const defaultList = getDefaultTokenList();
 
-    defaultList.pop();
+  console.log(defaultList);
 
-    return (
-        <SwapWidget
-            onSwap={onSwap}
-            onDisConnect={onDisConnect}
-            width={"500px"}
-            connection={{
-                AccountId: accountId || "",
-                isSignedIn: !!accountId,
-            }}
-            className="mx-auto"
-            transactionState={{
-                state: swapState,
-                setState: setSwapState,
-                tx,
-                detail: "(success details show here)",
-            }}
-            defaultTokenList={defaultList as TokenMetadata[]}
-            enableSmartRouting={true}
-            onConnect={onConnect}
-        />
-    );
+  //   defaultList.pop();
+
+  return (
+    <SwapWidget
+      onSwap={onSwap}
+      onDisConnect={onDisConnect}
+      width={"500px"}
+      connection={{
+        AccountId: accountId || "",
+        isSignedIn: !!accountId,
+      }}
+      className="mx-auto"
+      transactionState={{
+        state: swapState,
+        setState: setSwapState,
+        tx,
+        detail: "(success details show here)",
+      }}
+      defaultTokenList={defaultList as TokenMetadata[]}
+      enableSmartRouting={true}
+      onConnect={onConnect}
+    />
+  );
 };
